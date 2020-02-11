320be87396427be55569a5b47f6dd6b7c222c166
Kazuhiro Misumi <misumi@brighton.abiko.denken.or.jp>
2020-02-11 19:12:27 +0900

    Modified to generalize to use calc_iso_surf.

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,6 +1,4 @@
-import sys
-sys.path.append("/Users/misumi/tower/mypython/tool") # this code is only for calc_iso_surf.so
-import calc_iso_surf
+from . import calc_iso_surf
 from . import misc
 from . import netcdf
 from . import density
